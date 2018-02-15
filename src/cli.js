c3c7fb0b84195a35e1c234b3932a47755b90bc8e
Fabio M. Costa <fabiomcosta@gmail.com>
2018-02-15 20:48:56 -0300

    better help message with example

--- src/cli.js
+++ src/cli.js
@@ -6,6 +6,8 @@ import yargs from 'yargs';
 import {executeTransform} from './runner';
 
 const { argv } = yargs
+  .usage('$0 - moves a JavaScript module and updates all import references in the project.')
+  .example('$0 ./a.js ./b.js', 'Moves `./a.js` to `./b.js` and updates every other module importing `./a.js` to now import `./b.js`.')
   .demandCommand(2)
   .help();
 
