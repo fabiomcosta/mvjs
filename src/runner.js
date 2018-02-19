456767bf4155f982fabfbd33154def7e2b475994
Fabio M. Costa <fabiomcosta@gmail.com>
2018-02-18 23:04:23 -0300

    actually moving the file

--- src/runner.js
+++ src/runner.js
@@ -3,6 +3,7 @@
 import child_process from 'child_process';
 import {promisify} from 'util';
 import path from 'path';
+import fs from 'fs';
 import {createDebug} from './log';
 import {findAllJSPaths, findProjectPath} from './path';
 import {validate, normalize} from './options';
@@ -10,6 +11,7 @@ import type {Options} from './options';
 
 const debug = createDebug(__filename);
 const spawn = promisify(child_process.spawn);
+const rename = promisify(fs.rename);
 
 export async function executeTransform(options: Options): Promise<string> {
 
@@ -27,7 +29,8 @@ export async function executeTransform(options: Options): Promise<string> {
   const allJSPaths = await findAllJSPaths(projectPath);
   debug('Detected js paths', `\n  ${allJSPaths.join('\n  ')}`);
 
-  // TODO MOVE FILE sourcePath -> targetPath
+    // TODO support multiple sources
+  const absoluteSourcePath = absoluteSourcePaths[0];
 
   const jscodeshiftBin = path.join(
     __dirname, '..', 'node_modules', '.bin', 'jscodeshift'
@@ -36,12 +39,12 @@ export async function executeTransform(options: Options): Promise<string> {
   const cmdArgs = [
     '--silent',
     '--transform', path.join(__dirname, 'transform.js'),
-    // TODO FOR NOW WE ONLY ACCEPT 1 SOURCE PATH
-    '--absoluteSourcePath', absoluteSourcePaths[0],
+    '--absoluteSourcePath', absoluteSourcePath,
     '--absoluteTargetPath', absoluteTargetPath,
     ...allJSPaths
   ];
 
+  await rename(absoluteSourcePath, absoluteTargetPath);
   return await spawn(
     jscodeshiftBin,
     cmdArgs,
