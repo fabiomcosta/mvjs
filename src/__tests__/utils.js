046fd30e6cf07709cfae6ae2a53740e7f9cfa3b4
Fabio M. Costa <fabiomcosta@gmail.com>
2018-03-04 11:14:27 -0800

    creating files on the tmp fs concurrently, which made the integration tests run significantly faster

--- src/__tests__/utils.js
+++ src/__tests__/utils.js
@@ -3,13 +3,14 @@
 import fs from 'fs';
 import os from 'os';
 import path from 'path';
+import _mkdirp from 'mkdirp';
 import _rimraf from 'rimraf';
 import {promisify} from 'util';
 import pkg from '../../package.json';
 import type {Context} from '../transform';
 
 const mkdtemp = promisify(fs.mkdtemp);
-const mkdir = promisify(fs.mkdir);
+const mkdirp = promisify(_mkdirp);
 const writeFile = promisify(fs.writeFile);
 const fsStat = promisify(fs.stat);
 const rimraf = promisify(_rimraf);
@@ -44,18 +45,22 @@ type FsDescriptor = {
 export async function createTemporaryFs(definition: FsDefinition): Promise<FsDescriptor> {
   const tmpFolderPrefix = `${pkg.name.replace(/\W/g, '_')}-`;
   const tmpFolder = await mkdtemp(path.join(os.tmpdir(), tmpFolderPrefix));
-  for (const [_path, content] of Object.entries(definition)) {
-    if (path.isAbsolute(_path)) {
-      throw new Error(`Paths should be relative, "${_path}" is absolute.`);
-    }
-    const absolutePath = path.join(tmpFolder, _path);
-    if (_path.endsWith('/')) {
-      await mkdir(absolutePath);
-    } else {
-      const text = await Promise.resolve(content);
-      await writeFile(absolutePath, text);
-    }
-  }
+
+  await Promise.all(Object.entries(definition)
+    .map(async ([_path, content]) => {
+      if (path.isAbsolute(_path)) {
+        throw new Error(`Paths should be relative, "${_path}" is absolute.`);
+      }
+      const absolutePath = path.join(tmpFolder, _path);
+      if (_path.endsWith('/')) {
+        await mkdirp(absolutePath);
+      } else {
+        await mkdirp(path.dirname(absolutePath));
+        const text = await Promise.resolve(content);
+        await writeFile(absolutePath, text);
+      }
+    }));
+
   // TODO: also remove when user exits with ctrl+c and other corner cases
   process.on('exit', () => rimraf.sync(tmpFolder));
   return {
