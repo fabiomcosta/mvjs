9aa0ef6b2c2268f8fbd6f9b0fb2a7e37fb0ca8b7
Fabio M. Costa <fabiomcosta@gmail.com>
2018-03-04 11:13:01 -0800

    adding more normalize unit tests

--- src/__tests__/options.test.js
+++ src/__tests__/options.test.js
@@ -3,17 +3,39 @@
 import path from 'path';
 import {normalize} from '../options';
 
+const join = path.join.bind(path, process.cwd());
+
 describe('normalize', () => {
-  test('normalizes sourcePaths and targetPaths into a PathMap', async () => {
-    const cwd = process.cwd();
+
+  test('renames one file sourcePath to its targetPaths', async () => {
     const pathMap = normalize({
-      sourcePaths: ['./foo.js', './a/bar.js'],
+      sourcePaths: ['./foo.js'],
+      targetPath: './bar.js'
+    });
+    expect(pathMap).toEqual({
+      [join('./foo.js')]: join('./bar.js'),
+    });
+  });
+
+  test('renames one directory sourcePath to its targetPaths', async () => {
+    const pathMap = normalize({
+      sourcePaths: ['./a/s/d'],
       targetPath: './baz'
     });
-    const join = path.join.bind(path, cwd);
+    expect(pathMap).toEqual({
+      [join('./a/s/d')]: join('./baz'),
+    });
+  });
+
+  test('normalizes sourcePaths and targetPaths into a PathMap', async () => {
+    const pathMap = normalize({
+      sourcePaths: ['./foo.js', './a/bar.js', './a/s/d'],
+      targetPath: './baz'
+    });
     expect(pathMap).toEqual({
       [join('./foo.js')]: join('./baz/foo.js'),
-      [join('./a/bar.js')]: join('./baz/bar.js')
+      [join('./a/bar.js')]: join('./baz/bar.js'),
+      [join('./a/s/d')]: join('./baz/d')
     });
   });
 });
