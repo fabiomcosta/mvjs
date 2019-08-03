24b668ffe9982173010f6192444dca853f570001
Fabio M. Costa <fabiomcosta@gmail.com>
2019-08-03 15:58:07 -0700

    Fixing nodejs "new Buffer" deprecation

--- src/base64.js
+++ src/base64.js
@@ -1,9 +1,9 @@
 // @flow
 
 export function objectToBase64(obj: Object): string {
-  return new Buffer(JSON.stringify(obj)).toString('base64');
+  return Buffer.from(JSON.stringify(obj)).toString('base64');
 }
 
 export function base64ToObject(base64String: string): Object {
-  return JSON.parse(new Buffer(base64String, 'base64').toString('utf8'));
+  return JSON.parse(Buffer.from(base64String, 'base64').toString('utf8'));
 }
